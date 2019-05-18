96089f16ddc08933400f21464fd753aebdf951e8
jiho <dream_cacao_jh@naver.com>
2019-05-18 16:36:35 +0900

    rand기능 추가

--- hangman.c
+++ hangman.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
+#include<ctype.h>
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
 
 int check(char s[], char a[], char ch);
-int rand_maker(char s1[], char s2[], char s3[], char s4[], char a[]);
-int rand_ans(char s[], char a[]);
+void rand_maker(char s1[], char s2[], char s3[], char s4[], char a[]);
+void rand_ans(char s[], char a[]);
 
 int main()
 {
 	int num;
 	char solution[100];
 	char s1[100] = "meet at midnight";
-	char s2[100] = "Hello world";
+	char s2[100] = "hello world";
 	char s3[100] = "gnu nice cs";
-	char s4[100] = "C programing is fun";
+	char s4[100] = "c programing is fun";
 
-	char answer[100] = "____ __ ________";
+	char answer[100];
 	char ch;
+
+	rand_maker(solution, s1, s2, s3, s4, answer);
 
 	while (1)
 	{
@@ -45,22 +48,43 @@ int check(char s[], char a[], char ch)
 	else return 0;                       //아니면 0 반환
 }
 
-int rand_maker(char solution[],char s1[],char s2[],char s3[],char s4[],char a[])
+void rand_maker(char solution[],char s1[],char s2[],char s3[],char s4[],char a[]) //4개의 문자열중 하나를 무작위로 지정 
 {
 	int num,i;
+
+	srand((unsigned)time(NULL));
+
 	num = rand() % 4 + 1;
 	
 	switch (num)
 	{
 	case 1:
-		rand_ans(s1, a);
+		strcpy(solution, s1);
 		break;
 	case 2:
-		rand_ans(s2, a);
+		strcpy(solution, s2);
+		break;
+	case 3:
+		strcpy(solution, s3);
+		break;
+	default:
+		strcpy(solution, s4);
 		break;
 	}
+	rand_ans(solution, a);
 }
-int rand_ans(char s[], char a[])
+void rand_ans(char s[], char a[]) // solution[]의 알파벳 부분을 '_'로 변환
 {
+	int i = 0;
+
+	while (s[i]!='\0')
+	{
+		if (isalpha(s[i]))
+			a[i] = '_';
+		else
+			a[i] = ' ';
+		i++;
+	}
+	a[i] = '\0';
 
 }
